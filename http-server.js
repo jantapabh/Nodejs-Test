361aefc6c31ffe290b47c51a979b5858d9a67bf2
jantapabh <jantapa2407@gmail.com>
2019-12-27 23:20:56 +0700

    edit8

--- http-server.js
+++ http-server.js
@@ -1,20 +1,14 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer(function (req, res) {
 
-// server.listen(3000);
-// console.log('Listen form port 3000');
-
-
-server.addListener('connection', function(req, res) {
-
-    if ( req.url === '/') {
+    if (req.url === '/') {
         res.write('You are at home page');
         res.end();
     }
-    // console.log('Client connector!');
+
 });
 
-server.addListener('connection', function(socket) {
+server.addListener('connection', function (socket) {
 
     console.log('Client connector!');
 });

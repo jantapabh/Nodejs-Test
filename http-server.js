364bfcd21cbdf5e16105bb77843f651cdca40b31
jantapabh <jantapa2407@gmail.com>
2019-12-27 23:11:29 +0700

    server test

--- http-server.js
+++ http-server.js
@@ -5,9 +5,20 @@ const server = http.createServer();
 // console.log('Listen form port 3000');
 
 
-server.addListener('connection', function (socket) {
+server.addListener('connection', function (req, res) {
+
+    if (url === '/') {
+        res.write('You are at home page');
+        res.rnd();
+    }
+    // console.log('Client connector!');
+});
+
+erver.addListener('connection', function (socket) {
+
     console.log('Client connector!');
 });
+
 
 server.listen(3000);
 console.log('Listen form port 3000');

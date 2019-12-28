95744ae4a0dcbe7a19b135533ea4982343a13e34
jantapabh <jantapa2407@gmail.com>
2019-12-28 15:32:56 +0700

    app.js test

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const { getSecretKey, databaseName } = require('./settings');
-console.log(getSecretKey);
-console.log(databaseName);
+const myAsync = (callback) => {
+    console.log('Start');
+
+    setTimeout(() => {
+        const result = callback('Hello');
+        console.log(result);
+
+    }, 2000);
+
+    console.log('Finish');
+}
+
+myAsync((message) => {
+    return message + 'Async.';
+});
